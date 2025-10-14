e9a474244135f5c9b6e74c21a4991c80342c4c12
ReoKM <reomessi658@gmail.com>
2025-10-14 12:49:15 +0900

    create PR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  // ↓ ここから下を追記
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+
+  googleFonts: {
+    families: {
+      // 'Noto Sans JP' を読み込む
+      'Noto Sans JP': [400, 700], // 400:標準, 700:太字
+    },
+    display: 'swap', // フォント読み込み中のテキストの表示方法
+  }
 })
